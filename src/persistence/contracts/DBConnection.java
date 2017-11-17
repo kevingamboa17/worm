548f4e83babee006c635818b202757c3552589c3
Adrian <aleyvaschz@gmail.com>
2017-11-16 21:35:29 -0600

    Implement database connection

--- src/persistence/contracts/DBConnection.java
+++ src/persistence/contracts/DBConnection.java
@@ -2,7 +2,7 @@ package persistence.contracts;
 
 import java.sql.Connection;
 
-interface DBConnection {
+public interface DBConnection {
     public Connection getConnection();
     public void closeConnection(Connection connection);
 }

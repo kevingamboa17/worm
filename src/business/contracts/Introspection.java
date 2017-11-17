bc27651c65b31a4d78b1bf65c9a3742125d500bf
Kevin Gamboa <kevin-gamboa1@hotmail.com>
2017-11-16 19:42:43 -0600

    Refactor the use of Hashmaps in contracts, Now uses FieldWormType

--- src/business/contracts/Introspection.java
+++ src/business/contracts/Introspection.java
@@ -2,8 +2,6 @@ package business.contracts;
 
 import domain.WormObject;
 
-import java.util.HashMap;
-
 public interface Introspection<GenericObject extends WormObject> {
-    GenericObject buildObject(GenericObject genericObject, HashMap<String, String> attributes);
+    GenericObject buildObject(GenericObject genericObject);
 }
